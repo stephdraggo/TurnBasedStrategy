ea0c600546435f368fe5f9eaecb1de9f9f48d043
stephdraggo <steph.valais@gmx.com>
2021-03-17 16:30:58 +1100

    different blue

---
+++ Assets/Scripts/BoardBuilding/UnitCube.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ocean.Board
+{
+    public class UnitCube : MonoBehaviour
+    {
+        private GameObject neighbourXPos, neighbourXNeg, neighbourYPos, neighbourYNeg, neighbourZPos, neighbourZNeg;
+        public GameObject Neighbour {  get; private set; }
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
